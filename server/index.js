258debf1d067fea81230359e22d166996a7cf7ba
Tom Mitchell <mitcht032@gmail.com>
2021-02-24 20:29:31 -0800

    hiding url password with .env

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
-
+const PORT = process.env.PORT || 5000;
 const app = express();
+
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
@@ -13,10 +15,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://mitch-t:Brontoe13@stayfit.w13ur.mongodb.net/image?retryWrites=true&w=majority";
-const PORT = process.env.PORT|| 5000;
+//const CONNECTION_URL = "mongodb+srv://mitch-t:Brontoe13@stayfit.w13ur.mongodb.net/image?retryWrites=true&w=majority";
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
